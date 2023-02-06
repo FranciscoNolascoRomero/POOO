3966089e52e1e3201bcd821eb2602a6cf154d1ab
FNR <francisnolascoromero@gmail.com>
2023-02-06 15:44:24 +0100

    jkaskdjas999aa

---
+++ src/Ejercicios/TestCilindro.java
@@ -0,0 +1,11 @@
+package Ejercicios;
+
+public class TestCilindro {
+    //coleccion para tres cilindros
+    //Scanner para leer datos
+    //leemos altura chequeamos que esa altura este entre 0,2 y 25,9
+    //leemos radio chequeamos que ese radio este entre 0,1 y 9.9
+    //mostramos datos
+    //mostramos cilindro de mayor volumen
+    //mostramos cilindro de menor superficie
+}
