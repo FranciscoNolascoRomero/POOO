9848e65c2b230b90687c93620bcc9fd1271f32bc
FNR <francisnolascoromero@gmail.com>
2023-02-17 15:43:28 +0100

    jkaskdjas999aaaa11aaa

--- src/ClasesInternas/Agenda.java
+++ src/ClasesInternas/Agenda.java
@@ -1,5 +1,9 @@
 package ClasesInternas;
 
+import teoria.Ejemplos.Persona;
+
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,8 +22,48 @@ public class Agenda {
     }
 
     //getters //
-    //metodo que añada un contacto
-    //metodo que elimine contactos
-    //metodo que actualize un contacto
-    //metodo que muestre un contacto,dado el telefono
+
+    public String getNombreAgenda() {
+        return nombreAgenda;
+    }
+
+    public List<Contacto> getContactos() {
+        return contactos;
+    }
+    public boolean annadirContacto(Contacto contacto){
+       return contactos.add(contacto);
+    }
+    public boolean eliminarContacto(Contacto contacto){
+        return contactos.remove(contacto);
+    }
+
+    public void setNombreAgenda(String nombreAgenda) {
+        this.nombreAgenda = nombreAgenda;
+    }
+
+    public Contacto mostrarlo(String telefono){
+        for (Contacto contacto:contactos) {
+            if (contacto.getTelefono().equals(telefono))
+                return contacto;
+        }
+        return null;
+    }
+
+    public boolean actualizarContacto(Contacto oldContacto,Contacto newContacto){
+
+        for (Contacto contacto:contactos) {
+            if (!oldContacto.equals(newContacto))
+              oldContacto = newContacto;
+            return true;
+
+        }
+        return false;
+    }
+
+    //metodo que devuelva una lista de objetos
+    public static int calcularEdad(LocalDate fecha){
+        LocalDate fechaActual = LocalDate.now();
+        Period period = Period.between(fechaActual,fecha);
+        return period.getYears();
+    }
 }
--- src/ClasesInternas/TestAgenda.java
+++ src/ClasesInternas/TestAgenda.java
@@ -1,7 +1,28 @@
 package ClasesInternas;
 
-public class TestAgenda {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
+public class TestAgenda {
+    public static void main(String[] args) throws IOException {
+        List<Contacto> contactos = new ArrayList<>();
+        List<String> lineas = Files.readAllLines(Paths.get("Datos/contactoscsv.csv"));
+        System.out.printf("Leidas %d lineas%n",lineas.size());
+        for (String linea:lineas) {
+            String[] tokens = linea.split(",");
+            //el token 0 = nombre
+            //el token 1 = apellidos
+            //el token 2 = telefono
+            //el token 3 = fechaNacimiento
+            //localDate.parse(tokenn3) y obtenemo un objeto LocalDate
+            //Creamos objeto Contacto
+            //LO añladimos a la lista contactos
+
+        }
+        //Con un scanner pedimos el nombre de la agenda
+        // con contactos y el nombre de la agenda creamos un objeto agenda
     }
 }
